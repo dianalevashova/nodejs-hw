07987497ba4cda3bf226566512ab63e2668461ec
Diana L <justpiano190@gmail.com>
2026-09-21 14:23:06 +0300

    fix: add pino-pretty dependency and configure pino-http transport

--- src/server.js
+++ src/server.js
@@ -9,7 +9,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
